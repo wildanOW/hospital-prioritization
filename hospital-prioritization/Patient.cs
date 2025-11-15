d339abc91ac6c6d7fd01863b4d806d9248e936e3
Wildan <wildan.owais2904@gmail.com>
2025-11-15 16:29:31 +0800

    Added optimized priority algorithm with Patient and HospitalPrioritySystem classes

---
+++ hospital-prioritization/Patient.cs
@@ -0,0 +1,16 @@
+﻿namespace hospital_prioritization
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        public int Severity { get; set; }
+        public int ArrivalOrder { get; set; }
+
+        public Patient(string name, int severity, int arrivalOrder)
+        {
+            Name = name;
+            Severity = severity;
+            ArrivalOrder = arrivalOrder;
+        }
+    }
+}
